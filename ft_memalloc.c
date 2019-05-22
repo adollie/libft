a4ee55fb5130bc7ac9eb2223bfe730d96ae1b977
adollie <adollie@student.wethinkcode.co.za>
2019-05-22 11:25:38 +0200

    memalloc

---
+++ ft_memalloc.c
@@ -0,0 +1,24 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_memalloc.c                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: adollie <marvin@42.fr>                     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2019/05/22 11:23:13 by adollie           #+#    #+#             */
+/*   Updated: 2019/05/22 11:25:13 by adollie          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+#include <stdlib.h>
+
+void	*ft_memalloc(size_t size)
+{
+	void	*mem;
+
+	if (!(mem = malloc(size)))
+		return (NULL);
+	ft_bzero(mem, size);
+	return (mem);
+}
