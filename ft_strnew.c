8b4aa103b18687fd876e3649dcec2968daecdf32
adollie <adollie@student.wethinkcode.co.za>
2019-05-22 11:32:43 +0200

    strnew

---
+++ ft_strnew.c
@@ -0,0 +1,26 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strnew.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: adollie <marvin@42.fr>                     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2019/05/22 11:29:05 by adollie           #+#    #+#             */
+/*   Updated: 2019/05/22 11:32:23 by adollie          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+char	*ft_strnew(size_t size)
+{
+	size_t	i;
+	char	*string;
+
+	if (!(string = ft_memalloc(size + 1)))
+		return (NULL);
+	i = 0;
+	while (i < size)
+		string[i++] = '\0';
+	return (string);
+}
