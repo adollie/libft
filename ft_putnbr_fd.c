b4d3d7e078ba7dd32403a5d5e75c1ef76e14489d
Ashieqa Dollie <adollie@c3r8s4.wethinkcode.co.za>
2019-05-23 14:34:04 +0200

    putnbr fd

---
+++ ft_putnbr_fd.c
@@ -0,0 +1,29 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_putnbr_fd.c                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: adollie <marvin@42.fr>                     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2019/05/23 14:30:47 by adollie           #+#    #+#             */
+/*   Updated: 2019/05/23 14:33:41 by adollie          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+void	ft_putnbr_fd(int nb, int fd)
+{
+	unsigned int	nbr;
+
+	if (nb < 0)
+	{
+		ft_putchar_fd('-', fd);
+		nbr = (unsigned int)(nb * -1);
+	}
+	else
+		nbr = (unsigned int)nb;
+	if (nbr >= 10)
+		ft_putnbr_fd(nbr / 10, fd);
+	ft_putchar_fd((char)(nb % 10 + 48), fd);
+}
