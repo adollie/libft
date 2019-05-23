74ae4b295cb079788815b1c2679887d99fd51d94
Ashieqa Dollie <adollie@c3r8s4.wethinkcode.co.za>
2019-05-23 09:43:08 +0200

    strsub

---
+++ ft_strsub.c
@@ -0,0 +1,23 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strsub.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: adollie <marvin@42.fr>                     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2019/05/23 09:31:39 by adollie           #+#    #+#             */
+/*   Updated: 2019/05/23 09:42:37 by adollie          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+char	*ft_strsub(char const *s, unsigned int start, size_t len)
+{
+	char	*result;
+
+	if (!s || !(result = ft_memalloc(len + 1)))
+		return (NULL);
+	result = ft_strncpy(result, (char*)s + start, len);
+	return (result);
+}
