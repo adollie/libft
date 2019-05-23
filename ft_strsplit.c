b05797acdd3eedf479f021da5e289febaadf68d6
Ashieqa Dollie <adollie@c3r8s4.wethinkcode.co.za>
2019-05-23 10:49:17 +0200

    strsplit

---
+++ ft_strsplit.c
@@ -0,0 +1,26 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strsplit.c                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: adollie <marvin@42.fr>                     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2019/05/23 10:46:00 by adollie           #+#    #+#             */
+/*   Updated: 2019/05/23 10:48:56 by adollie          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stdlib.h>
+#include "libft.h"
+
+char	**ft_strsplit(char const *s, char c)
+{
+	char	*charset;
+
+	if (!s || !c)
+		return (NULL);
+	if (!(charset = ft_strnew(2)))
+		return (NULL);
+	charset[0] = c;
+	return (ft_strsplitchrset((char*)s, charset));
+}
