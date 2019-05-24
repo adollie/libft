f7221149e0909a3aa4e2807f0feeafd88f294bee
Ashieqa Dollie <adollie@c3r8s4.wethinkcode.co.za>
2019-05-24 11:14:18 +0200

    modified

--- ft_memcmp.c
+++ ft_memcmp.c
@@ -6,21 +6,27 @@
 /*   By: adollie <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/05/20 12:12:57 by adollie           #+#    #+#             */
-/*   Updated: 2019/05/20 12:15:55 by adollie          ###   ########.fr       */
+/*   Updated: 2019/05/24 11:13:57 by adollie          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-void	*ft_memchr(const void *s, int c, size_t n)
+int	ft_memcmp(const void *s1, const void *s2, size_t n)
 {
 	size_t			i;
 	unsigned char	*ptr;
+	unsigned char	*ptr2;
 
+	if (n == 0)
+		return (0);
+	ptr = (unsigned char*)s1;
+	ptr2 = (unsigned char*)s2;
 	i = 0;
-	ptr = (unsigned char*)s;
-	while (++i <= n)
-		if (*(ptr++) == (unsigned char)c)
-			return ((void*)--ptr);
-	return (0);
+	while (*ptr == *ptr2 && ++i < n)
+	{
+		ptr++;
+		ptr2++;
+	}
+	return ((int)(*ptr - *ptr2));
 }
