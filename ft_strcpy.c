a03ce8be83d858e67eeb9e94cdbf4a2b17481612
Ashieqa Dollie <adollie@c3r8s4.wethinkcode.co.za>
2019-05-27 13:12:39 +0200

    updated

--- ft_strcpy.c
+++ ft_strcpy.c
@@ -6,9 +6,11 @@
 /*   By: adollie <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/05/21 09:54:18 by adollie           #+#    #+#             */
-/*   Updated: 2019/05/21 09:56:24 by adollie          ###   ########.fr       */
+/*   Updated: 2019/05/27 12:22:18 by adollie          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+
+#include "libft.h"
 
 char	*ft_strcpy(char *dest, const char *src)
 {
