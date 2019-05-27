6ae431f68ac1b28b8f4b5ffb192386f3fc0f7c6c
Ashieqa Dollie <adollie@c3r8s4.wethinkcode.co.za>
2019-05-27 16:12:29 +0200

    modifed

--- ft_strlcat.c
+++ ft_strlcat.c
@@ -6,35 +6,31 @@
 /*   By: adollie <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/05/21 10:17:59 by adollie           #+#    #+#             */
-/*   Updated: 2019/05/21 10:26:27 by adollie          ###   ########.fr       */
+/*   Updated: 2019/05/27 13:56:16 by adollie          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-size_t	ft_strlcat(char *dest, const char *src, size_t size)
+size_t		ft_strlcat(char *dst, const char *src, size_t size)
 {
-	size_t i;
-	size_t j;
-	size_t res;
+	size_t		altdst;
+	size_t		altsrc;
+	size_t		i;
 
+	if (!size)
+		return (ft_strlen(dst) + size);
+	altsrc = ft_strlen(src);
 	i = 0;
-	while (dest[i] != '\0')
-		++i;
-	res = 0;
-	while (src[res] != '\0')
-		++res;
-	if (size <= i)
-		res += size;
-	else
-		res += i;
-	j = 0;
-	while (src[j] != '\0' && i + 1 < size)
-	{
-		dest[i] = src[j];
+	while (dst[i] && i < size)
 		i++;
-		j++;
+	altdst = i;
+	while (src[i - altdst] && i < size - 1)
+	{
+		dst[i] = src[i - altdst];
+		i++;
 	}
-	dest[i] = '\0';
-	return (res);
+	if (altdst < size)
+		dst[i] = '\0';
+	return (altdst + altsrc);
 }
