d859ede826ca75c0c7d098672d0372adcd6bf842
Ashieqa Dollie <adollie@c3r8s4.wethinkcode.co.za>
2019-05-27 13:13:09 +0200

    updated

--- ft_strcmp.c
+++ ft_strcmp.c
@@ -6,16 +6,21 @@
 /*   By: adollie <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/05/22 09:46:03 by adollie           #+#    #+#             */
-/*   Updated: 2019/05/22 09:49:29 by adollie          ###   ########.fr       */
+/*   Updated: 2019/05/27 13:02:09 by adollie          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+
+#include "libft.h"
 
 int		ft_strcmp(const char *s1, const char *s2)
 {
 	int	i;
 
 	i = 0;
-	while (s1[i] && s2[i] && s1[i] == s2[i])
-		++i;
-	return ((unsigned char)s1[i] - (unsigned char)s2[i]);
+	while (*s1 && (*s1 == *s2))
+	{
+		s1++;
+		s2++;
+	}
+	return ((unsigned char)*s1 - (unsigned char)*s2);
 }
