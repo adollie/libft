b2752cdf6c3343ccdcd89f5127b2bc18f5303212
Ashieqa Dollie <adollie@c3r8s4.wethinkcode.co.za>
2019-05-27 16:12:01 +0200

    modified

--- ft_strncat.c
+++ ft_strncat.c
@@ -6,27 +6,28 @@
 /*   By: adollie <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/05/21 10:08:15 by adollie           #+#    #+#             */
-/*   Updated: 2019/05/21 10:17:08 by adollie          ###   ########.fr       */
+/*   Updated: 2019/05/27 13:51:11 by adollie          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-char	*ft_strncat(char *dest, const char *src, size_t nb)
+char	*ft_strncat(char *restrict s1, const char *restrict s2, size_t n)
 {
-	int		i;
-	size_t	j;
+	char	*s1alt;
+	size_t	i;
 
+	s1alt = s1;
 	i = 0;
-	while (dest[i] != '\0')
-		i++;
-	j = 0;
-	while (src[i] != '\0' && j < nb)
+	while (*s1alt != '\0')
+		s1alt++;
+	while (i != n && *s2 != '\0')
 	{
-		dest[i] = src[j];
+		*s1alt = *s2;
+		s1alt++;
+		s2++;
 		i++;
-		j++;
 	}
-	dest[i] = '\0';
-	return (dest);
+	*s1alt = '\0';
+	return (s1);
 }
