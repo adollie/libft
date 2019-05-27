c6fbd5784cc97d3e82efdfa597fd82c32d0561ac
Ashieqa Dollie <adollie@c3r8s4.wethinkcode.co.za>
2019-05-27 16:14:06 +0200

    modifed

--- ft_strstr.c
+++ ft_strstr.c
@@ -6,29 +6,32 @@
 /*   By: adollie <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/05/22 09:32:42 by adollie           #+#    #+#             */
-/*   Updated: 2019/05/22 09:37:10 by adollie          ###   ########.fr       */
+/*   Updated: 2019/05/27 14:04:50 by adollie          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "libft.h"
+
 char	*ft_strstr(const char *str, const char *to_find)
 {
-	unsigned int	pos;
-	unsigned int	i;
+	size_t		i;
+	size_t		z;
 
-	if (!*to_find)
-		return ((char*)s);
-	pos = 0;
-	while (str[pos] != '\0')
+	i = 0;
+	if (*to_find == '\0')
+		return ((char *)str);
+	while (str[i] != '\0')
 	{
-		if (str[pos] == to_find[0])
+		z = 0;
+		while (to_find[z] == str[i + z])
 		{
-			i = 1;
-			while (to_find[i] != '\0' && str[pos + 1] == to_find[i])
-				++i;
-			if (to_find[i] == '\0')
-				return ((char*)&str[pos]);
+			if (to_find[z + 1] == '\0')
+			{
+				return ((char *)str + i);
+			}
+			z++;
 		}
-		++pos;
+		i++;
 	}
 	return (0);
 }
