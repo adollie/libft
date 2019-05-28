d2c4040f3993e3724e4c70add838d61c9f431e7d
Ashieqa Dollie <adollie@c3r8s4.wethinkcode.co.za>
2019-05-28 08:26:40 +0200

    modified

--- ft_strtrim.c
+++ ft_strtrim.c
@@ -6,29 +6,57 @@
 /*   By: adollie <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/05/23 10:34:56 by adollie           #+#    #+#             */
-/*   Updated: 2019/05/23 10:45:25 by adollie          ###   ########.fr       */
+/*   Updated: 2019/05/27 15:39:49 by adollie          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-char	*ft_strtrim(char const *s)
+static int	whatspace(int c)
 {
-	char			*result;
-	unsigned int	start;
-	unsigned int	size;
-	unsigned int	spaces_count;
+	if ((c == ' ' || c == '\n' || c == '\t'))
+		return (1);
+	return (0);
+}
 
-	if (!s)
+static char	strspace(char const *s)
+{
+	int		i;
+	int		z;
+
+	i = 0;
+	z = 0;
+	while (s[i] != '\0')
+	{
+		if ((s[i] == ' ' || s[i] == '\n' || s[i] == '\t'))
+			z++;
+		i++;
+	}
+	if (i == z)
+		return (1);
+	return (0);
+}
+
+char		*ft_strtrim(char const *s)
+{
+	size_t		front;
+	size_t		back;
+	size_t		z;
+	char		*middle;
+
+	if (!s || !ft_strlen(s))
+		return ((char*)s);
+	if (strspace(s) == 1)
+		return (ft_strnew(1));
+	front = 0;
+	back = ft_strlen(s) - 1;
+	z = 0;
+	while (whatspace(s[front]))
+		front++;
+	while (whatspace(s[back]) != 0)
+		back--;
+	z = back - front + 1;
+	if (!(middle = ft_strnew(z + 1)))
 		return (NULL);
-	size = (unsigned int)ft_strlen((char*)s);
-	spaces_count = ft_strncntfirstchar((char*)s, " \n\t");
-	start = spaces_count;
-	if (size > spaces_count)
-		spaces_count += ft_strcntlastchar((char*)s, " \n\t");
-	size -= spaces_count;
-	if (!(result = ft_memalloc((size_t)size + 1)))
-		return (NULL);
-	result = ft_strncpy(result, (char*)s + start, size);
-	return (result);
+	return (ft_strncpy(middle, &s[front], z));
 }
